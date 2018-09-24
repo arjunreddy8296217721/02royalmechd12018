876bfe6f305fa0509451368dedd23999141a7a06
arjunreddy8296217721 <42932981+arjunreddy8296217721@users.noreply.github.com>
2018-09-24 14:43:22 +0530

    Add files via upload

---
+++ lbs4.py
@@ -0,0 +1,13 @@
+num=int(input("enter the number"))
+if(num>=90):
+  print("A+")
+elif(num>=80):
+ print("A")
+elif(num>=70):
+ print("B+")
+elif(num>=60):
+ print("B")
+elif(num>=40):
+ print("c")
+else:
+  print("fail")
---
+++ lbs43.py
@@ -0,0 +1,7 @@
+a=b=c=int("input(value of a,b&c"))
+if(a>b&a>c):
+	print("a is greater than b&c")
+elif(b>c):
+	print("b is greater than a&c")
+else:
+	print("c is greater than b&a")
