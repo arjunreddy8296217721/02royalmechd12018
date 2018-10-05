9c59bb4e5d8f0eefc90927cb002ae3e986003343
arjunreddy8296217721 <42932981+arjunreddy8296217721@users.noreply.github.com>
2018-10-05 11:08:48 +0530

    Add files via upload

---
+++ lbs7.py
@@ -0,0 +1,6 @@
+'''l=[1,2,3,4]
+for i in l:
+	print(i)'''
+for i in range(1,11):
+	for j in range(1,11):
+		print(i,'x' ,j,"=",i*j)
---
+++ lbs73.py
@@ -0,0 +1,6 @@
+n=int(input("enter the table to be printed"))
+for i in range(n,n+1):
+	for j in range(1,11):
+		print(i,'x' ,j,"=",i*j)
+lst=[1000,7899,4,6777777]
+print(min)
