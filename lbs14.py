75dc0a76b4e9d825d53a6fc34fb4fb4b516282c3
arjunreddy8296217721 <42932981+arjunreddy8296217721@users.noreply.github.com>
2018-11-05 16:00:09 +0530

    Add files via upload

---
+++ lbs14.py
@@ -0,0 +1,9 @@
+class student:
+	age=18
+	name='king'
+	def display(ob):
+		c=ob.age
+		d=ob.name
+		print(c,'\n',d)
+a=student()
+a.display()
---
+++ lbs142.py
@@ -0,0 +1,9 @@
+class student:
+	usn=18
+	name='king'
+	def display(ob):
+		c=ob.usn
+		d=ob.name
+		print(c,'\n',d)
+a=student()
+a.display()
